e826b912472ceeb922f07d5249a741319a25ae59
Aakash-Naykude <aakashnaykude123@gmail.com>
2022-01-04 22:37:34 +0530

    make it better

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const app = require("./index");
-const connect = require("./configs/db");
+const app = require("./src/index");
+const connect = require("./src/configs/db");
 app.listen(process.env.PORT || 4000, async function () {
   await connect();
   console.log("Listening on port 4000");
