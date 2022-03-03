dc1828277b3dfc2ee6f5ba51aa29cd5c3008958c
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2022-03-03 21:30:36 +0530

    bit changes try

--- src/index.js
+++ src/index.js
@@ -7,14 +7,15 @@ const makeupController = require("./controllers/makeupproducts.controller");
 app.use("/makeup", makeupController);
 const skincareController = require("./controllers/skincareproducts.controller");
 app.use("/skincare", skincareController);
-const haircareController = require("./controllers/haircare.controller");
-app.use("/haircare", haircareController);
 const perfumeController = require("./controllers/perfume.controller");
 app.use("/perfume", perfumeController);
 const mensskincareController = require("./controllers/mensskincare.controller");
 app.use("/mensskincare", mensskincareController);
 const menscologneController = require("./controllers/menscologne.controller");
 app.use("/menscologne", menscologneController);
+
+const haircareController = require("./controllers/haircare.controller");
+app.use("/haircare", haircareController);
 
 const cartController = require("./controllers/cartpage.controller");
 app.use("/cart", cartController);
